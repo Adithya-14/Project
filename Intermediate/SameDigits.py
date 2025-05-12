04cd26eaf9c8a1130ea3c8883eaca8605a128685
Adithya-14 <aadiadithya620@gmail.com>
2025-05-12 09:50:54 +0530

    ReAdded the untermediate file

--- Intermediate/SameDigits.py
+++ Intermediate/SameDigits.py
@@ -1,17 +1,16 @@
-n=int(input("enter a number to find the same digits: "))
+n=int(input("ENter a number to find the same digits: "))
 def same_digits(n):
-    n1=n
-    n2=n
-    count=0
+    l=n%10
+    a=True
     while n>0:
         digit=n%10
+        if digit!=l:
+            a=False
+            break
         n//=10
-        count+=1
-    while n1>0:
-        digit=n1%10
-        n1//=10
-        if digit==count:
-            return f"{n2} does not have {count} same digits"
-    return f"{n2} has {count} same digits"
+    return a
 result = same_digits(n)
-print(result)
+if result:
+    print(f"The number {n} has same digits.")
+else:
+    print(f"The number {n} does not have same digits.")
